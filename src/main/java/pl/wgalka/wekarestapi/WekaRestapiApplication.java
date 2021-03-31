4d7b0bff1f417b352f65364a17f4e824d4ec4b2d
wgalka <wgalka2@op.pl>
2021-03-31 16:58:46 +0200

    spring inicializer

---
+++ src/main/java/pl/wgalka/wekarestapi/WekaRestapiApplication.java
@@ -0,0 +1,13 @@
+package pl.wgalka.wekarestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WekaRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WekaRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wgalka/wekarestapi/WekaRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wgalka.wekarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WekaRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
